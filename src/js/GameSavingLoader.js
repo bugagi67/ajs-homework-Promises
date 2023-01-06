faf62f538d74dd8946564fb69e0c50d9e790fc34
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2023-01-06 20:41:31 +0300

    initial commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,19 @@
+import read from './reader';
+import json from './parser';
+import GameSaving from './GameSaving';
+
+export default class GameSavingLoader {
+  static load() {
+    return new Promise((resolve) => {
+      const data = read();
+      data
+        .then((response) => {
+          const value = json(response);
+          return value;
+        })
+        .then((response) => {
+          resolve(new GameSaving(JSON.parse(response)));
+        });
+    });
+  }
+}
